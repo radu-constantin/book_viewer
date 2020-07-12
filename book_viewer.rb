28a552b1b6d69f6edada87ed28a7f1776d9120ed
Radu Constantin <raduadrian.constantin94@gmail.com>
2020-07-12 22:00:29 +0300

    Added search function

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,7 +2,61 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+helpers do
+  def format_text(text)
+    text.split(/\n\n/).map do |line|
+      "<p>#{line}</p>"
+    end.join
+  end
+end
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+not_found do
+  redirect "/"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
+
   erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter  = File.read ("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << {number: number, name: name} if contents.include?(query)
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
 end
